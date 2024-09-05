5672d0c3c4f7ee242a2d85e8dc6a4d76c1f45e78
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-09-05 13:51:25 -0600

    TDD show page

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,13 +1,32 @@
 require "rails_helper"
 
-RSpec.describe "index" do
-  it "displays a link to play game" do
-    visit "/"
-    expect(page).to have_content("Guess the bird by the sound!")
-    expect(page).to have_link("Bird Call Quiz")
-    click_link("Bird Call Quiz")
-    expect(page).to have_current_path("/game")
+describe "Game show page", type: :feature do
+  let!(:bird) { Bird.create(
+                            name: "Pine Siskin", 
+                            sound_file: "/sounds/XC880311 - Pine Siskin - Spinus pinus.mp3", 
+                            image_url: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/306420291/1800", 
+                            info_url: "https://ebird.org/species/pinsis"
+                            ) }
+
+  before do
+    visit game_path
   end
 
+  it "displays the bird's sound file" do
+    expect(page).to have_selector("audio")
+    expect(page).to have_css("audio > source[src='/sounds/XC880311 - Pine Siskin - Spinus pinus.mp3']")
+  end
 
+  it "displays a form to submit a guess" do
+    expect(page).to have_selector("form")
+    expect(page).to have_field("guess")
+    expect(page).to have_button("Submit Guess")
+  end
+
+  it "displays a link to learn more about the bird" do
+    expect(page).to have_link("Learn More about the Sparrow", href: bird.info_url)
+
+    learn_more_link = find_link("Learn More about the Pine Siskin")
+    expect(learn_more_link[:target]).to eq("_blank")
+  end
 end
---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "welcome" do
+  it "displays a link to play game" do
+    visit "/"
+    expect(page).to have_content("Guess the bird by the sound!")
+    expect(page).to have_link("Bird Call Quiz")
+    click_link("Bird Call Quiz")
+    expect(page).to have_current_path("/game")
+  end
+end
